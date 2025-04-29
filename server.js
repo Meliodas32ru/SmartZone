5ff8cf7baba6a8952530c0f2fe34304be86bca83
Daniil < email>
2025-04-29 21:15:18 +0300

    Initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3059;
+const PORT = 3069;
 
 mongoose.connect('mongodb://localhost:27017/SmartZone', {
     useNewUrlParser: true,
